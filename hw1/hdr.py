57bad9f4ae15b262a6964ae67e7f2c653e2786d0
shaoyu0966 <b06705028@ntu.edu.tw>
2021-04-01 13:45:36 +0800

    compute response curve

--- hw1/hdr.py
+++ hw1/hdr.py
@@ -37,7 +37,7 @@ class HDR:
         self.width = values.shape[2]
     
     def __close_to_center(self, rs, cs, radius):
-        # compute the Manhattan distances to the center
+        # compute the distances to the center
         # select one random point with dist < min(height, width)*radius or the one closest to the center
         center_r = self.height / 2
         center_c = self.width / 2
@@ -52,7 +52,8 @@ class HDR:
         best_dist = np.inf
         best_idx = -1
         for index, (r, c) in enumerate(zip(rs, cs)):
-            dist = np.abs(r - center_r) + np.abs(c - center_c)
+            # dist = np.abs(r - center_r) + np.abs(c - center_c)
+            dist = np.sqrt((r - center_r)**2 + (c - center_c)**2)
             if dist < thresh:
                 return r, c
             elif dist < best_dist:
@@ -62,7 +63,7 @@ class HDR:
 
     def __construct_Z(self):
         # self.z[i, j] = value of the j-th sampled pixel in the i-th image
-        self.z = np.zeros((self.n_image, self.n_sample))
+        self.z = np.zeros((self.n_image, self.n_sample), dtype=np.uint8)
         for img_idx in range(self.n_image):
             for sample_idx in range(self.n_sample):
                 self.z[img_idx, sample_idx] = self.values[img_idx, self.sample_pts[sample_idx][0], self.sample_pts[sample_idx][1]]
@@ -70,10 +71,7 @@ class HDR:
     def __weight(self, value, z_min=0, z_max=255):
         if value < z_min or value > z_max:
             raise RuntimeError(f'Invalid value {value}.')
-        thresh = (z_min + z_max) / 2
-        if value < thresh:
-            return value - z_min
-        return z_max - value
+        return min(value - z_min, z_max - value)
 
     def sample_pixels(self, n_sample, sample_radius=0.8, plot_hist=False, visualize_sample_pt=False):
         # compute the variance of values in all images
@@ -84,11 +82,13 @@ class HDR:
         
         # select the image with the largest variance
         self.sample_img_idx = np.argmax(variances)
+        print(f'Sample pixels based on image with index {self.sample_img_idx}')
 
         # compute the values with evenly spread percentiles
         percentiles = np.linspace(0, 100, num=n_sample)
         sample_values = np.percentile(self.values[self.sample_img_idx], percentiles, interpolation='nearest')
-        if plot_hist:
+        print(f'Sampled pixel values:\n{sample_values}\n')
+        if plot_hist: 
             plt.clf()
             plt.hist(self.values[self.sample_img_idx].reshape(-1), bins=np.arange(0, 260, 5))
             for i in range(len(sample_values)):
@@ -111,19 +111,55 @@ class HDR:
         
         # construct self.z
         self.__construct_Z()
+        print(f'Z:\n{self.z}\n')
     
-    def response_curve(self):
-        # RHS of the linear system
-        rhs = np.tile(self.ln_shutter_speed, self.n_sample)
-        
-        # LHS of the linear system
+    def response_curve(self, smoothing_lambda, visualize_g=False):
+        mat_A = np.zeros((self.n_image*self.n_sample + 255, 256 + self.n_sample), dtype=np.float64)
+        mat_b = np.zeros((mat_A.shape[0], 1), dtype=np.float64)
+        print('mat_A.shape:', mat_A.shape)
+        print('mat_b.shape:', mat_b.shape)
+
+        cur_r = 0
         # data-fitting constraints
-        
+        for i in range(self.n_sample):
+            for j in range(self.n_image):
+                # i-th sampled pixel in the j-th image
+                z = self.z[j, i]
+                w = self.__weight(z)
+                mat_A[cur_r, z] = w
+                mat_A[cur_r, 256 + i] = -1 * w
+                mat_b[cur_r] = w * self.ln_shutter_speed[j]
+                cur_r += 1
+
         # curve centering constraint
-        
+        mat_A[cur_r, 127] = 1
+        cur_r += 1
+
         # smoothing constraints
+        for z in range(1, 255):
+            w = self.__weight(z)
+            mat_A[cur_r, z-1] = w * smoothing_lambda
+            mat_A[cur_r, z] = -2 * w * smoothing_lambda
+            mat_A[cur_r, z+1] = w * smoothing_lambda
+            cur_r += 1
+
+        # solve the linear system
+        print('Computing the response curve...')
+        pseudo_inv_A = np.linalg.pinv(mat_A)
+        x = np.dot(pseudo_inv_A, mat_b)
+        inv_response_curve = x[:256, 0]
+
+        # visualization
+        if visualize_g:
+            plt.clf()
+            plt.scatter(inv_response_curve, np.arange(0, 256))
+            plt.title('Response Curve')
+            plt.xlabel('log exposure (E_i * (delta t)_j)')
+            plt.ylabel('pixel value (Z_ij)')
+            plt.show()
+
+        return inv_response_curve
         
-    
 
 
 
@@ -137,6 +173,7 @@ class HDR:
 
 n_sample_pt = 50
 sample_radius = 0.8
+smoothing_lambda = 50
 
 if __name__ == '__main__':
 
@@ -144,16 +181,16 @@ if __name__ == '__main__':
     input_image_dir = './input_images/corner/'
     input_image_paths = os.listdir(input_image_dir)
     input_image_paths = list(map(lambda f: input_image_dir+f, input_image_paths))
-    input_image_paths = list(filter(lambda f: f.endswith('.JPG'), input_image_paths))
+    input_image_paths = list(filter(lambda f: (f.endswith('.JPG') or f.endswith('.png')), input_image_paths))
     all_b, all_g, all_r = read_images(input_image_paths)
     
     # parse shutter speed
     shutter = np.array(list(map(parse_shutter_speed, input_image_paths)))
-    print('shutter speed: ', shutter)
+    print('Shutter speed:\n', shutter, '\n')
 
     # HDR for channel G
     hdr = HDR(shutter, all_g)
-    hdr.sample_pixels(n_sample_pt, sample_radius=sample_radius, visualize_sample_pt=False)
-    hdr.response_curve()
+    hdr.sample_pixels(n_sample_pt, sample_radius=sample_radius, plot_hist=False, visualize_sample_pt=False)
+    hdr.response_curve(smoothing_lambda, visualize_g=True)
 
     
