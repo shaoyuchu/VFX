86ebf3d8eef2d2555fa873e6ef0d0e10868f3770
shaoyu0966 <b06705028@ntu.edu.tw>
2021-04-27 11:23:12 +0800

    convert to grayscale

--- ImageStitching/code/harris.py
+++ ImageStitching/code/harris.py
@@ -3,6 +3,15 @@ import cv2
 import argparse
 from util import *
 
+class HarrisCornerDetector:
+
+    def __init__(self, image):
+        # convert to grayscale
+        if image.ndim == 3:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        self.image = image
+
+
 if __name__ == '__main__':
 
     # parse command line arguments
@@ -16,4 +25,9 @@ if __name__ == '__main__':
 
     # read images
     image_paths = image_paths_under_dir(input_dir)
-    print(image_paths)
+    for file_name in image_paths:
+        image = cv2.imread(f'{input_dir}/{file_name}')
+        harris = HarrisCornerDetector(image)
+
+
+        break
